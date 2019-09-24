6e65dee8596ad5ff0667acd981ea8f771fe0b63c
Èric Canela <knela96@gmail.com>
2019-09-24 17:33:50 +0200

    deleting files

--- Application.cpp
+++ Application.cpp
@@ -9,7 +9,6 @@ Application::Application()
 	renderer3D = new ModuleRenderer3D(this);
 	gui = new ModuleGUI(this);
 	camera = new ModuleCamera3D(this);
-	physics = new ModulePhysics3D(this);
 	
 	// The order of calls is very important!
 	// Modules will Init() Start() and Update in this order
@@ -20,7 +19,6 @@ Application::Application()
 	AddModule(camera);
 	AddModule(input);
 	AddModule(audio);
-	AddModule(physics);
 	
 	// Scenes
 	AddModule(scene_intro);
--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -1,7 +1,6 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleSceneIntro.h"
-#include "Primitive.h"
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -32,10 +31,6 @@ bool ModuleSceneIntro::CleanUp()
 // Update: draw background
 update_status ModuleSceneIntro::Update(float dt)
 {
-	Plane p(0, 1, 0, 0);
-	p.axis = true;
-	p.Render();
-
 	return UPDATE_CONTINUE;
 }
 
