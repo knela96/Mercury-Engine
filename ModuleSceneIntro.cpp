e28c57f2ff954f986c7ab52ed281cf79a02d5411
Èric Canela <knela96@gmail.com>
2019-09-26 18:14:46 +0200

    changed imgui version

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -1,7 +1,7 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleSceneIntro.h"
-#include "MathGeoLib/include/Geometry/Sphere.h"
+//#include "MathGeoLib/include/Geometry/Sphere.h"
 #include "Primitive.h"
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
