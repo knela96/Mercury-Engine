c9f30b44cc4faafb1d7bbcf65592a292b4f47c1e
Jaumeavinyo <36154250+Jaumeavinyo@users.noreply.github.com>
2019-10-21 00:48:07 +0200

    Memmory manager
    5 unresolved externals

--- Globals.h
+++ Globals.h
@@ -23,6 +23,10 @@ void log(const char file[], int line, const char* format, ...);
 
 
 typedef unsigned int uint;
+typedef unsigned char uchar;
+typedef unsigned __int32 uint32;
+typedef unsigned __int64 uint64;
+
 
 enum update_status
 {
